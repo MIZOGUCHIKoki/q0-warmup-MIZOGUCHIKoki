f5145b91602a9163dac7bca7a4cc6e64b03582fd
MIZOGUCHI Koki <250373b@ugs.kochi-tech.ac.jp>
2022-10-11 15:36:47 +0900

    edited

--- chap3/521.s
+++ chap3/521.s
@@ -3,14 +3,14 @@
   global  _start
 
 _start:
-  mov   ebx, 5    ; sum
-  mov   ecx, 1    ; i
+  mov   ebx, 0    ; sum
+  mov   ecx, 5    ; i
 
 loop0:
   add   ebx, ecx  ; ebx = ebx + ecx
   inc   ecx       ; i++
   cmp   ecx, 21   ; ecx compare 21 
-  jle   loop0     ; Jump if Less or Equal (ecx <= 10)
+  jle   loop0     ; Jump if Less or Equal (ecx <= 21)
 
   mov   eax, 1    
   int   0x80
