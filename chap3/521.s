437969be71d06f476f954c0827c0e6d18c925d15
MIZOGUCHI Koki <250373b@ugs.kochi-tech.ac.jp>
2022-10-11 14:22:08 +0900

    create file 521.s

--- chap3/521.s
+++ chap3/521.s
@@ -1,1 +1,16 @@
-  ;5以上10以下の整数の和
+  ;5以上21以下の整数の和を計算する．
+  section .text
+  global  _start
+
+_start:
+  mov   ebx, 5    ; sum
+  mov   ecx, 1    ; i
+
+loop0:
+  add   ebx, ecx  ; ebx = ebx + ecx
+  inc   ecx       ; i++
+  cmp   ecx, 21   ; ecx compare 21 
+  jle   loop0     ; Jump if Less or Equal (ecx <= 10)
+
+  mov   eax, 1    
+  int   0x80
